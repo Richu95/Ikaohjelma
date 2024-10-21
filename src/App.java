8d748bb84f196cf0fa0ab5889f1d266e6c1f7f98
Riku Turunen <riku.turunen@student.hamk.fi>
2024-10-21 15:08:25 +0300

    Aloitus

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+ int ika = 16;
+
+ if (ika == 18) {
+     System.out.println("olet juuri tullut taysi-ikaiseksi, saat ajaa autoa");
+    
+ }
+ if (ika == 20 || ika == 30 || ika == 40 || ika == 50 || ika == 60 || ika == 70 || ika == 80 || ika == 90 || ika == 100){
+     System.out.println("Onnea tasavuosikymmenestä!");
+ }
+ if (ika == 100) {
+     System.out.println("Paljon");
+     System.out.println("onnea");
+     System.out.println("sinulle");
+ }
 }
+}
+
